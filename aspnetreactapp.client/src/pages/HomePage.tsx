5cea201d5e30f75a3e78c5ba48e8eca6d8227683
趙世平 <sam_chao@i-planet.com.tw>
2024-07-10 17:22:46 +0800

    v1.0.4

--- aspnetreactapp.client/src/pages/HomePage.tsx
+++ aspnetreactapp.client/src/pages/HomePage.tsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useRef, useState } from "react";
+const text = ["This Is Samuel Chao... Welcome...", "I am a Web Developer..."];
 
 const HomePage: React.FC = () => {
-  const [visibleChars, setVisibleChars] = useState(0);
   const containerRef = useRef<HTMLDivElement>(null);
-  const text = "This Is Samuel Chao... Welcome...";
-
+  const initiateText = "Please Scroll To Start...";
+  const [visibleChars, setVisibleChars] = useState(initiateText.length);
+  const [showText, setShowText] = useState(initiateText);
   useEffect(() => {
     const handleScroll = () => {
       if (containerRef.current) {
-        const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
-        const scrollPercentage =
-          (scrollTop / (scrollHeight - clientHeight)) * 100;
-        const newVisibleChars = Math.floor(
-          (scrollPercentage / 100) * text.length
-        );
-        console.log(newVisibleChars);
-        setVisibleChars(Math.min(newVisibleChars, text.length));
+        // const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
+        const { scrollTop, scrollHeight } = containerRef.current;
+        const portion = scrollHeight / text.length;
+        const index = Math.floor(scrollTop / portion);
+        const percentage = ((scrollTop % portion) / portion) * 100;
+        if (scrollTop === 0) {
+          setShowText(initiateText);
+          setVisibleChars(initiateText.length);
+        }
+        console.log(percentage);
+        if (scrollTop > 0) {
+          setShowText(text[index]);
+          setVisibleChars(
+            Math.floor((text[index].length * percentage) / 100 + 1)
+          );
+        }
       }
     };
 
@@ -29,25 +38,22 @@ const HomePage: React.FC = () => {
         container.removeEventListener("scroll", handleScroll);
       }
     };
-  }, [text.length]);
+  }, []);
 
   return (
     <div
       ref={containerRef}
       className="h-[calc(100vh-6rem)] overflow-y-auto hide-scrollbar"
     >
-      <div className="h-[200%] relative">
+      <div className={`h-[${text.length}000%] relative`}>
         <h1 className="w-full text-4xl font-bold sticky top-1/2 -translate-y-1/2 pt-4 text-center word-spacing-[1rem] tracking-widest">
-          {visibleChars === 0 && (
-            <>
-              <span className="animate-fade-in-out-none">
-                Please Scroll To Start...
-              </span>
-            </>
-          )}
-        </h1>
-        <h1 className="w-full text-4xl font-bold sticky top-1/2 -translate-y-1/2 pt-4 text-center word-spacing-[1rem] tracking-widest">
-          {text.split("").map((char, index) => (
+          {/* {showText.split("").map((char, index) => (
+            <>{index < visibleChars && <span key={index}>{char}</span>}</>
+          ))} */}
+          {showText.split("").map((char, index) => {
+            if (index < visibleChars) return <span key={index}>{char}</span>;
+          })}
+          {/* {showText.split("").map((char, index) => (
             <span
               key={index}
               className={`transition-opacity duration-300 ease-in-out ${
@@ -56,7 +62,7 @@ const HomePage: React.FC = () => {
             >
               {char}
             </span>
-          ))}
+          ))} */}
         </h1>
       </div>
     </div>
